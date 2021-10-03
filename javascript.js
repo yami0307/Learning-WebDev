4ff1c5be00b3edd76557d666464bcbed7659eef0
yami0307 <48398798+yami0307@users.noreply.github.com>
2021-10-03 19:49:51 +0530

    Next Commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//Some string Functions in JS
+
+var favShow = "Game of Thrones";
+console.log(favShow.length)
+
+console.log(favShow[0])
+
+console.log(favShow[favShow.length-1])
+
+console.log(favShow.indexOf("of"))
+
+console.log(favShow.slice(5,7))
+
+var srk = "My name is Khan."
+console.log(srk.toUpperCase())
+console.log(srk.toLowerCase())
---
+++ javascript2.js
@@ -0,0 +1,23 @@
+//User-defined datatypes
+
+// Objects
+
+let animal = {
+    name: "Cow",
+    legs: 4,
+}
+//Dot Notation
+console.log(animal.name);
+//Bracket Notation
+let legsprop = "legs";
+console.log(animal["legs"]);
+console.log(animal[legsprop]);
+
+
+//Arrays
+let selectBooks = ["The Rudest Book Ever","Think and Grow rich","Invisible Man"];
+console.log(selectBooks[0])
+console.log(selectBooks[1])
+console.log(selectBooks[2])
+
+
